3fda070090392ee7cc2920863b78ca4c941a58af
Krupadv <krupa.ecourierz@gmail.com>
2020-12-01 18:40:12 +0530

    push second

---
+++ src/test/java/testPrograms/Addprogram.java
@@ -0,0 +1,22 @@
+package testPrograms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class Addprogram 
+{
+	@Test
+	public static void methodOne()
+	{
+		//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\chromedriver.exe");
+		//	WebDriver driver= new ChromeDriver();
+
+//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\geckodriver.exe");
+//		WebDriver driver= new FirefoxDriver();
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\IEDriverServer.exe");
+		WebDriver driver= new InternetExplorerDriver();
+		driver.get("https://www.google.com/");
+	}
+}
