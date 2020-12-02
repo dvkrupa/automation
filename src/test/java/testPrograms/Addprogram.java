39e44096678575b18b70def12fb7a9904bac323c
dvkrupa <d.v.krupa@gmail.com>
2020-12-02 16:35:13 +0530

    launch browser

--- src/test/java/testPrograms/Addprogram.java
+++ src/test/java/testPrograms/Addprogram.java
@@ -13,8 +13,9 @@ public class Addprogram
 		//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\chromedriver.exe");
 		//	WebDriver driver= new ChromeDriver();
 
-//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\geckodriver.exe");
-//		WebDriver driver= new FirefoxDriver();
+		//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\geckodriver.exe");
+		//		WebDriver driver= new FirefoxDriver();
+
 		System.setProperty("webdriver.ie.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\IEDriverServer.exe");
 		WebDriver driver= new InternetExplorerDriver();
 		driver.get("https://www.google.com/");
