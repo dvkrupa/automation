149927210835e6b2f371be044fba66560db1b1ff
dvkrupa <d.v.krupa@gmail.com>
2020-12-02 17:50:20 +0530

    moving launching browser

--- src/test/java/testPrograms/Addprogram.java
+++ src/test/java/testPrograms/Addprogram.java
@@ -1,23 +1,33 @@
 package testPrograms;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class Addprogram 
 {
+	static WebDriver driver;
 	@Test
-	public static void methodOne()
+	public static void launchingBrowser()
 	{
-		//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\chromedriver.exe");
-		//	WebDriver driver= new ChromeDriver();
-
-		//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\geckodriver.exe");
-		//		WebDriver driver= new FirefoxDriver();
-
-		System.setProperty("webdriver.ie.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\IEDriverServer.exe");
-		WebDriver driver= new InternetExplorerDriver();
+		String browser="chrome";
+		//CHROME BROWSER
+		if(browser=="chrome") {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\chromedriver.exe");
+			driver= new ChromeDriver();
+		}
+		//FIREFOX BROWSER
+		if(browser=="firefox") {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\geckodriver.exe");
+			driver= new FirefoxDriver();
+		}
+		//INTERNET EXPLORER BROWSER
+		if(browser=="ie") {
+			System.setProperty("webdriver.ie.driver", "C:\\Users\\Krupa\\eclipse-workspace\\javaprograms\\Driversfolder\\IEDriverServer.exe");
+			driver= new InternetExplorerDriver();
+		}
 		driver.get("https://www.google.com/");
 	}
 }
